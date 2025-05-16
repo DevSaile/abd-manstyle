7be107b26cb442413cc953a5c4f539b20209009b
AshiRu <ashktaru@gmail.com>
2025-05-16 16:17:55 -0600

    Agregadas animaciones a las paginas restantes

--- FrontEnd_MansStyle/src/pages/admin/Compras.jsx
+++ FrontEnd_MansStyle/src/pages/admin/Compras.jsx
@@ -76,7 +76,10 @@ const BuyingPage = () => {
   };
 
   return (
-    <motion.div className="flex-1 overflow-auto relative z-10">
+    <motion.div className="flex-1 overflow-auto relative z-10"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}>
       <Header title="Buying" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
